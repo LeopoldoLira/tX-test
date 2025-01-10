ce08a982ea4a2f95f021dfc07b13663a7a6b619e
Leopoldo Lira <lira.leopoldo97@gmail.com>
2025-01-09 21:37:06 -0600

    create (theme)!: added front-page.php template to display the content of the landing page.

---
+++ front-page.php
@@ -0,0 +1,5 @@
+<!-- This loads the header.php file at the root of the theme. By doing this all of the content of the header.php file will be load to this template and get all of the dynamic data. -->
+<? get_header();?>
+
+<!-- This loads the footer.php file at the root of the theme. By doing this all of the content of the footer.php file will be load to this template and get all of the dynamic data. -->
+<? get_footer();?>
