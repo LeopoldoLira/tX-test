bb7973c012c96a02d19705872c43160dd97c7334
Leopoldo Lira <lira.leopoldo97@gmail.com>
2025-01-09 22:07:11 -0600

    task (theme): added theme_styles function in the scripts.php file.

--- includes/scripts.php
+++ includes/scripts.php
@@ -4,4 +4,16 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+// Registering and loading style scripts from the CSS folder to load the scripts across the whole website.
+
+function theme_styles(){
+    wp_register_style('main_stylesheet', THEMEROOT . '/assets/css/main.min.css', '', '1.0', 'all');
+    wp_register_style('normalize',THEMEROOT . '/assets/css/normalize.css', '', '1.0', 'all');
+    wp_enqueue_style('normalize');
+    wp_enqueue_style('main_stylesheet');
+
+}
+
+add_action('wp_enqueue_scripts','theme_styles');
+
 ?>
