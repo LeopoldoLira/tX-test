d3a68c461c4853f605de61034ce089f0ac3ea4eb
Leopoldo Lira <lira.leopoldo97@gmail.com>
2025-01-09 21:47:31 -0600

    create (theme)!: added a basic template for the functions.php file in the theme.

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?
+
+// Exit if accessed directly, to prevent Unauthorized access and following better security practices.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+//Disabling Default emojis scripts from wordpress for better performance since scripts are not needed for this theme.
+function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );  
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );  
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+}
+add_action( 'init', 'disable_emojis' );
+?>
