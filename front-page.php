70a9f179c73f84337e15bb9148ae2042fe37b87a
Leopoldo Lira <lira.leopoldo97@gmail.com>
2025-01-11 15:23:49 -0600

    task (front-page.php)!: added both mobile and desktop Fibre section

--- front-page.php
+++ front-page.php
@@ -1,9 +1,25 @@
 <!-- This loads the header.php file at the root of the theme. By doing this all of the content of the header.php file will be load to this template and get all of the dynamic data. -->
 <? get_header();?>
 
-<section>
-    
-
+<section class='fibre-container'>
+ <div class='fibre-container__home'>
+    <div class='info'>
+        <h2>Fibre to the home</h2>
+        <p>With our fibre-to-the-home service, you can enjoy fast speeds without any shared connections. Experience uninterrupted, high-speed internet with no lag or downtime, ensuring a smooth and reliable online experience.</p>
+        <button class='primary-button'>Upgrade to Fibre today</button>
+    </div>
+    <picture class='img'>
+        <img src='http://thrillx-test.local/wp-content/uploads/2025/01/front-store-photo.webp' />
+    </picture>
+ </div>
+ <div class='fibre-container__area' style='background-image:url("http://thrillx-test.local/wp-content/uploads/2025/01/house-background-scaled.webp"); background-position:center; background-size:cover'>
+    <div class='content'>
+        <h2>Check for Fibre in your area</h2>
+        <p>Discover how our Fibre network can enhance your connectivity experience with clear and reliable service tailored to your needs.</p>
+        <button class='primary-button desktop-button'>Check Availability</button>
+        <button class='primary-button mobile-button'>Find fibre now!</button>
+    </div>
+ </div>
 </section>
 
 
