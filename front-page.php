b980402dde26ac88018218549092c9a04e05a891
Leopoldo Lira <lira.leopoldo97@gmail.com>
2025-01-12 22:27:45 -0600

    task (front-page.php)!: added both mobile and desktop styles/content for hero section slider.

--- front-page.php
+++ front-page.php
@@ -1,6 +1,45 @@
 <!-- This loads the header.php file at the root of the theme. By doing this all of the content of the header.php file will be load to this template and get all of the dynamic data. -->
 <? get_header();?>
 
+<!-- Slider main container -->
+<div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide" style='background-image:url("/wp-content/themes/thrillx/resources/imgs/slide-one.png")'>
+        <div class='swiper-slide-content'> 
+            <h2>Experience local and reliable connectivity you can trust</h2>
+            <p>Fast, reliable internet, phone, TV, and mobility for home or business. Enjoy local support in just 13 seconds—connectivity made easy.</p>
+            <button class='secondary-button' >Sign-up Today</button>
+        </div>
+    </div>
+    <div class="swiper-slide" style='background-image:url("/wp-content/themes/thrillx/resources/imgs/slide-two.jpeg")'>
+        <div  class='swiper-slide-content'>
+            <h2>Experience local and reliable connectivity you can trust</h2>
+            <p>Fast, reliable internet, phone, TV, and mobility for home or business. Enjoy local support in just 13 seconds—connectivity made easy.</p>
+            <button class='secondary-button' >Sign-up Today</button>
+        </div>
+    </div>
+    <div class="swiper-slide" style='background-image:url("/wp-content/themes/thrillx/resources/imgs/slide-three.jpeg")'>
+        <div  class='swiper-slide-content'>
+            <h2>Experience local and reliable connectivity you can trust</h2>
+            <p>Fast, reliable internet, phone, TV, and mobility for home or business. Enjoy local support in just 13 seconds—connectivity made easy.</p>
+            <button class='secondary-button' >Sign-up Today</button>
+        </div>
+    </div>
+    
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
+
+  <div class='swiper-player-button'>
+    <span class='pause-button'>Pause<button id='pause-button'><img src='/wp-content/themes/thrillx/resources/icons/pause.svg'/></button></span>
+    <span class='play-button'>Resume<button id='play-button'><img src='/wp-content/themes/thrillx/resources/icons/play.svg'/></button></span>
+  </div>
+
+</div>
+
+
 <section class='fibre-container'>
  <div class='fibre-container__home'>
     <div class='info'>
@@ -22,6 +61,32 @@
  </div>
 </section>
 
+<section class='capabilities-container'>
+    <div class='capabilities-container__commitment'>
+        <div class='capabilities-img'>
+            <img src="/wp-content/uploads/2025/01/capability-image-one-scaled.webp" alt="" />
+        </div>
+        <div class='capabilities-content'>
+            <div>
+                <h2>Harness Amazon’s reach without the headache</h2>
+                <p>We take care of the marketplace so you can spend more time on your core competency.</p>
+                <button class='primary-button'>Sign-up Today</button>
+            </div>
+        </div>
+    </div>
+    <div class='capabilities-container__accordion'>
+        <div class='capabilities-content'>
+            <div>
+                <h2>Keep full brand control while we do the heavy lifting</h2>
+                <p>We take care of the marketplace so you can spend more time on your core competency.</p>
+                <button class='primary-button'>Sign-up Today</button>
+            </div>
+        </div>
+        <div class='capabilities-img'>
+            <img src="/wp-content/uploads/2025/01/capability-image-two-scaled.webp" alt="" />
+        </div>
+    </div>
+</section>
 
 
 <section class='requirements-container'>
