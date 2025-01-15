e2117d27e8d057e93fbb02c8b6296374ef5b42af
Leopoldo Lira <lira.leopoldo97@gmail.com>
2025-01-15 07:21:05 -0600

    update (modal-ajax-package-.js)!: initialized ACF witht the function to get expected behaviour

--- includes/postal-code-script.php
+++ includes/postal-code-script.php
@@ -1,24 +1,46 @@
 <?php
-// Retrieve the postal codes from the ACF options page
-$fiber_postal_codes = array_column(get_field('fibre_postal_codes', 'option'), 'postal_code'); // Extract 'postal_code' field
-$non_fiber_postal_codes = array_column(get_field('non_fibre_postal_codes', 'option'), 'postal_code'); // Extract 'postal_code' field
-
-function load_packages_by_zip() {
-    global $fiber_postal_codes, $non_fiber_postal_codes; // Access global variables
-
-    $postal_code = $_POST['postal_code']; // Get the postal code from AJAX request
-
-    // Check the postal code and load the appropriate template part
-    if (in_array($postal_code, $fiber_postal_codes)) {
-        get_template_part('template-parts/fiber-packages');
-    } elseif (in_array($postal_code, $non_fiber_postal_codes)) {
-        get_template_part('template-parts/non-fiber-packages');
-    } else {
-        get_template_part('template-parts/non-serviceable-areas');
+function initialize_acf_and_load_packages() {
+    // Helper function to extract postal codes
+    function extract_postal_codes($field_data) {
+        $postal_codes = [];
+        if (is_array($field_data)) {
+            foreach ($field_data as $item) {
+                if (isset($item['postal_code'])) {
+                    $postal_codes[] = $item['postal_code'];
+                }
+            }
+        }
+        return $postal_codes;
     }
-    wp_die();
+
+    // Store the postal codes in a persistent location (closure scope)
+    $fiber_postal_codes = extract_postal_codes(get_field('fibre_postal_codes', 'option'));
+    $non_fiber_postal_codes = extract_postal_codes(get_field('non_fibre_postal_codes', 'option'));
+
+    // AJAX handler function
+    function load_packages_by_zip() {
+        // Retrieve postal codes dynamically in the AJAX context
+        $fiber_postal_codes = extract_postal_codes(get_field('fibre_postal_codes', 'option'));
+        $non_fiber_postal_codes = extract_postal_codes(get_field('non_fibre_postal_codes', 'option'));
+
+        $postal_code = isset($_POST['postal_code']) ? sanitize_text_field($_POST['postal_code']) : '';
+
+        // Validate postal code and load the appropriate template
+        if (in_array($postal_code, $fiber_postal_codes)) {
+            get_template_part('template-parts/fiber-packages');
+        } elseif (in_array($postal_code, $non_fiber_postal_codes)) {
+            get_template_part('template-parts/non-fiber-packages');
+        } else {
+            get_template_part('template-parts/non-serviceable-areas');
+        }
+        wp_die();
+    }
+
+    // Register the AJAX actions
+    add_action('wp_ajax_load_packages', 'load_packages_by_zip');
+    add_action('wp_ajax_nopriv_load_packages', 'load_packages_by_zip');
 }
 
-add_action('wp_ajax_load_packages', 'load_packages_by_zip');
-add_action('wp_ajax_nopriv_load_packages', 'load_packages_by_zip');
+// Hook everything to ACF initialization
+add_action('acf/init', 'initialize_acf_and_load_packages');
 ?>
